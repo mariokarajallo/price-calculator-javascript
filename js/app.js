4ffd1b4020cc12dfcab5d821ea87e2b1cdc6652d
mariokarajallo <mario_karajallo@hotmail.com>
2022-09-14 20:32:14 -0400

    creamos el constructor UI y cargamos año en HTML

--- js/app.js
+++ js/app.js
@@ -4,3 +4,35 @@ function Seguro(marca, year, tipo) {
   this.year = year;
   this.tipo = tipo;
 }
+// a la hora de instanciarlos no pasaremos nada al constructor
+// creamos la funcion por que de otra manera no podremos agregar prototype
+function UI() {}
+
+//llena las opciones de los anos
+// como no usaremos this, entonces podemos usar arrow function
+UI.prototype.llenarOpciones = () => {
+  const max = new Date().getFullYear(),
+    min = max - 20;
+
+  // seleccionamos el selector de anho
+  const selectYear = document.querySelector("#year");
+
+  // cargamos los anhos en el selector
+  for (let i = max; i > min; i--) {
+    // creamos el elemento "option "para el selector
+    let option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+
+    //agregamos en el HTML
+    selectYear.appendChild(option);
+  }
+};
+
+//instanciar UI
+const ui = new UI();
+console.log(ui);
+
+//! EVENT LISTENER
+document.addEventListener("DOMContentLoaded", () => {});
+//! FUNCIONES
